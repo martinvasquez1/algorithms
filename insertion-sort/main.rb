9c69defe5aa2d07624eb067c9642faf37970e91c
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-08-09 11:21:35 -0400

    Add insertion sort

---
+++ insertion-sort/main.rb
@@ -0,0 +1,17 @@
+def insertion_sort(a)
+    (1...a.length).each do |j|
+        key = a[j]
+
+        i = j - 1
+        while i > 0 && a[i] > key
+            a[i + 1] = a[i]
+            i = i - 1
+        end
+
+        a[i + 1] = key
+    end
+end
+
+arr1 = [6, 9, 1]
+insertion_sort(arr1)
+p arr1
