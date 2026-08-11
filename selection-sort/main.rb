cda664b455b151d6cbf8df45a97f3e4572279629
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-08-10 21:51:38 -0400

    Add selection sort

---
+++ selection-sort/main.rb
@@ -0,0 +1,20 @@
+def selection_sort(array)
+    (0...array.length - 1).each do |i|
+        current_number = array[i]
+        smallest_index = i
+
+        (i...array.length).each do |j|
+            if array[j] < array[smallest_index]
+                smallest_index = j
+            end
+        end
+
+        array[i] = array[smallest_index]
+        array[smallest_index] = current_number
+    end
+
+    array
+end
+
+array1 = [4, 7, 2, 3, 9]
+result = selection_sort(array1)
